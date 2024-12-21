cae5a0f0d4bae022d02cce7a33703204a9cb6b0d
El-Chaball.Code <marcosminier29@gmail.com>
2024-12-21 07:59:31 -0600

    Add files via upload

---
+++ Desafios Windows-Form/Windows-Form Desafio-03/Desafio W-Form-03/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Desafio_W_Form_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtDivision = new System.Windows.Forms.TextBox();
+            this.txtResta = new System.Windows.Forms.TextBox();
+            this.txtMultiplicacion = new System.Windows.Forms.TextBox();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.lblNum1.ForeColor = System.Drawing.Color.White;
+            this.lblNum1.Location = new System.Drawing.Point(64, 27);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(79, 20);
+            this.lblNum1.TabIndex = 0;
+            this.lblNum1.Text = "Numero 1";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.lblNum2.ForeColor = System.Drawing.Color.White;
+            this.lblNum2.Location = new System.Drawing.Point(334, 27);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(79, 20);
+            this.lblNum2.TabIndex = 1;
+            this.lblNum2.Text = "Numero 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(63, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(64, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Suma";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(334, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Division";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(334, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Multiplicacion";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.BackColor = System.Drawing.Color.White;
+            this.txtNum2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNum2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.ForeColor = System.Drawing.Color.Black;
+            this.txtNum2.Location = new System.Drawing.Point(338, 50);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(141, 14);
+            this.txtNum2.TabIndex = 6;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.BackColor = System.Drawing.Color.White;
+            this.txtNum1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNum1.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.txtNum1.ForeColor = System.Drawing.Color.Black;
+            this.txtNum1.Location = new System.Drawing.Point(68, 50);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(141, 14);
+            this.txtNum1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnCalcular);
+            this.panel1.Controls.Add(this.txtNum1);
+            this.panel1.Controls.Add(this.txtNum2);
+            this.panel1.Controls.Add(this.lblNum2);
+            this.panel1.Controls.Add(this.lblNum1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.panel1.Location = new System.Drawing.Point(127, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(545, 148);
+            this.panel1.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnCalcular.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalcular.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(231, 99);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(87, 27);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel2.Controls.Add(this.btnLimpiar);
+            this.panel2.Controls.Add(this.txtDivision);
+            this.panel2.Controls.Add(this.txtResta);
+            this.panel2.Controls.Add(this.txtMultiplicacion);
+            this.panel2.Controls.Add(this.txtSuma);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(127, 190);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(545, 231);
+            this.panel2.TabIndex = 9;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.Orange;
+            this.btnLimpiar.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpiar.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnLimpiar.Location = new System.Drawing.Point(213, 182);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(125, 27);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Borrar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtDivision
+            // 
+            this.txtDivision.BackColor = System.Drawing.Color.White;
+            this.txtDivision.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDivision.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.txtDivision.ForeColor = System.Drawing.Color.Black;
+            this.txtDivision.Location = new System.Drawing.Point(338, 122);
+            this.txtDivision.Name = "txtDivision";
+            this.txtDivision.Size = new System.Drawing.Size(141, 14);
+            this.txtDivision.TabIndex = 12;
+            // 
+            // txtResta
+            // 
+            this.txtResta.BackColor = System.Drawing.Color.White;
+            this.txtResta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResta.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.txtResta.ForeColor = System.Drawing.Color.Black;
+            this.txtResta.Location = new System.Drawing.Point(68, 122);
+            this.txtResta.Name = "txtResta";
+            this.txtResta.Size = new System.Drawing.Size(141, 14);
+            this.txtResta.TabIndex = 11;
+            // 
+            // txtMultiplicacion
+            // 
+            this.txtMultiplicacion.BackColor = System.Drawing.Color.White;
+            this.txtMultiplicacion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMultiplicacion.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.txtMultiplicacion.ForeColor = System.Drawing.Color.Black;
+            this.txtMultiplicacion.Location = new System.Drawing.Point(338, 48);
+            this.txtMultiplicacion.Name = "txtMultiplicacion";
+            this.txtMultiplicacion.Size = new System.Drawing.Size(141, 14);
+            this.txtMultiplicacion.TabIndex = 10;
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.BackColor = System.Drawing.Color.White;
+            this.txtSuma.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSuma.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.txtSuma.ForeColor = System.Drawing.Color.Black;
+            this.txtSuma.Location = new System.Drawing.Point(68, 48);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.Size = new System.Drawing.Size(141, 14);
+            this.txtSuma.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(10)))), ((int)(((byte)(61)))));
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtDivision;
+        private System.Windows.Forms.TextBox txtResta;
+        private System.Windows.Forms.TextBox txtMultiplicacion;
+        private System.Windows.Forms.TextBox txtSuma;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Desafios Windows-Form/Windows-Form Desafio-03/Desafio W-Form-03/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_W_Form_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtNum1.Text);
+            double num2 = double.Parse(txtNum2.Text);
+            double suma, resta, multiplicacion, division;
+
+
+
+            suma = num1 + num2;
+            resta = num1 - num2;
+            multiplicacion = num1 * num2;
+            division = num1 / num2;
+
+
+            txtSuma.Text = suma.ToString();
+            txtResta.Text = resta.ToString();
+            txtMultiplicacion.Text = multiplicacion.ToString();
+            txtDivision.Text = division.ToString();
+
+
+
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Clear();
+            txtNum2.Clear();
+            txtSuma.Clear();
+            txtResta.Clear();
+            txtMultiplicacion.Clear();
+            txtDivision.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
