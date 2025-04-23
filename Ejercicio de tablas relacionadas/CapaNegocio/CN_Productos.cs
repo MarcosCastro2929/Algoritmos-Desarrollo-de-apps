a42c3a289323c2745a43b7617a3fa535e795dfb9
El-Chaball.Code <marcosminier29@gmail.com>
2025-04-22 16:05:47 -0800

    Add files via upload

---
+++ Ejercicio de tablas relacionadas/CapaNegocio/CN_Productos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class CN_Productos
+    {
+        private CD_Productos objetoCD = new CD_Productos();
+
+        public DataTable MostrarProd()
+        {
+            return objetoCD.Mostrar();
+        }
+
+        public void InsertarProd(string nombre, string desc, string marca, string precio, string stock, string categoriaId)
+        {
+            objetoCD.Insertar(nombre, desc, marca, Convert.ToDouble(precio), Convert.ToInt32(stock), Convert.ToInt32(categoriaId));
+        }
+
+        public void EditarProd(string nombre, string desc, string marca, string precio, string stock, string categoriaId, string id)
+        {
+            objetoCD.Editar(nombre, desc, marca, Convert.ToDouble(precio), Convert.ToInt32(stock), Convert.ToInt32(categoriaId), Convert.ToInt32(id));
+        }
+
+        public void EliminarProd(string id)
+        {
+            objetoCD.Eliminar(Convert.ToInt32(id));
+        }
+    }
+
+    public class CN_Categorias
+    {
+        private CD_Categorias objetoCD = new CD_Categorias();
+
+        public DataTable MostrarCategorias()
+        {
+            return objetoCD.MostrarCategorias();
+        }
+    }
+}
+
