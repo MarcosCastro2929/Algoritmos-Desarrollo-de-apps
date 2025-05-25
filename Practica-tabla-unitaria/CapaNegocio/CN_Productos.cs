e4f94e4287e431498482c24ade67ced801287f11
El-Chaball.Code <marcosminier29@gmail.com>
2025-05-25 11:52:37 -0800

    Add files via upload

---
+++ Practica-tabla-unitaria/CapaNegocio/CN_Productos.cs
@@ -0,0 +1,51 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class CN_Productos
+    {
+
+
+        private CD_Productos objetoCD = new CD_Productos();
+
+        public DataTable MostrarProd()
+        {
+            return objetoCD.Mostrar();
+        }
+
+
+        
+        public void InsertarProd(string nombre, string desc, string marca, string precio, string stock, int estado)
+        {
+            objetoCD.Insertar(nombre, desc, marca, Convert.ToDouble(precio), Convert.ToInt32(stock), estado);
+        }
+
+
+
+
+        public void EditarProd(string nombre, string desc, string marca, string precio, string stock, string id)
+        {
+            objetoCD.Editar(nombre, desc, marca, Convert.ToDouble(precio), Convert.ToInt32(stock), Convert.ToInt32(id));
+        }
+
+
+
+
+        public void EliminarProd(string id)
+        {
+            objetoCD.Eliminar(Convert.ToInt32(id));
+        }
+
+
+
+
+
+    }
+}
