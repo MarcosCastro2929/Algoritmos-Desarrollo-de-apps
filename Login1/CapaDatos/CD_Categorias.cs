b8a528af78ddc381e268e90e53ed5066e41efc45
El-Chaball.Code <marcosminier29@gmail.com>
2025-05-25 13:49:26 -0800

    Add files via upload

---
+++ Login1/CapaDatos/CD_Categorias.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CapaDatos
+{
+    public class CD_Categorias
+    {
+        public DataTable MostrarCategorias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class CD_Categoria
+        {
+
+            private CD_Conexion conexion = new CD_Conexion();
+            DataTable tabla = new DataTable();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader leer;
+
+            public DataTable Mostrar()
+            {
+                comando.Connection = conexion.AbrirConexion();
+                comando.CommandText = "sp_ObtenerCategorias";
+                comando.CommandType = CommandType.StoredProcedure;
+                leer = comando.ExecuteReader();
+                tabla.Load(leer);
+                conexion.CerrarConexion();
+                return tabla;
+            }
+
+
+
+            public void Insertar(string nombre)
+            {
+                comando.Connection = conexion.AbrirConexion();
+                comando.CommandText = "InsertarCategoria";  // Asegúrate de que el procedimiento esté creado
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@Nombre", nombre);
+
+                comando.ExecuteNonQuery();
+                comando.Parameters.Clear();
+                conexion.CerrarConexion();
+            }
+
+
+            public void Editar(int id, string nombre)
+            {
+                comando.Connection = conexion.AbrirConexion();
+                comando.CommandText = "EditarCategoria"; // Asegúrate de que este procedimiento exista en tu BD
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@Id", id);
+                comando.Parameters.AddWithValue("@Nombre", nombre);
+
+                comando.ExecuteNonQuery();
+                comando.Parameters.Clear();
+                conexion.CerrarConexion();
+            }
+
+
+            public void Eliminar(int id)
+            {
+                comando.Connection = conexion.AbrirConexion();
+                comando.CommandText = "EliminarCategoria"; // Asegúrate de que este procedimiento exista en tu BD
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@Id", id); // Usando 'Id' ya que en Categorias es el nombre del campo
+                comando.ExecuteNonQuery();
+                comando.Parameters.Clear();
+                conexion.CerrarConexion();
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Login1/CapaDatos/CD_Usuarios.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class CD_Usuarios
+    {
+        private string connectionString = "Server=DESKTOP-G79D1BT\\SQLEXPRESS;DataBase= Practica_De_Inventario_De_Productos;Integrated Security=true";
+
+        public bool Login(string usuario, string contraseña)
+        {
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                SqlCommand comando = new SqlCommand("sp_Login", conexion);
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@contraseña", contraseña);
+
+                conexion.Open();
+                SqlDataReader reader = comando.ExecuteReader();
+                return reader.HasRows;
+            }
+        }
+
+   
+        public string ObtenerRol(string usuario, string contraseña)
+        {
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                SqlCommand comando = new SqlCommand("sp_Login", conexion);
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@contraseña", contraseña);
+
+                conexion.Open();
+                SqlDataReader reader = comando.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    return reader["Rol"].ToString();
+                }
+
+                return null;
+            }
+        }
+
+        public int RegistrarUsuario(string usuario, string contraseña, string rol)
+        {
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                SqlCommand comando = new SqlCommand("sp_RegistrarUsuario", conexion);
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@contraseña", contraseña);
+                comando.Parameters.AddWithValue("@rol", rol);
+
+                conexion.Open();
+                return Convert.ToInt32(comando.ExecuteScalar());
+            }
+        }
+
+       
+        public int RegistrarUsuarioConRol(string usuario, string contraseña, string rol)
+        {
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                SqlCommand comando = new SqlCommand("sp_RegistrarUsuario", conexion);
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@contraseña", contraseña);
+                comando.Parameters.AddWithValue("@rol", rol);
+
+                conexion.Open();
+                return Convert.ToInt32(comando.ExecuteScalar());
+            }
+        }
+
+        public bool ExisteUsuario(string usuario)
+        {
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                SqlCommand comando = new SqlCommand(
+                    "SELECT COUNT(*) FROM Usuarios WHERE Usuario = @usuario",
+                    conexion);
+
+                comando.Parameters.AddWithValue("@usuario", usuario);
+
+                conexion.Open();
+                int count = Convert.ToInt32(comando.ExecuteScalar());
+                return count > 0;
+            }
+        }
+
+
+        public string ObtenerRolDesdeBD(string usuario)
+        {
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+               
+           
+                string rol = "";
+
+                using (SqlCommand cmd = new SqlCommand("SELECT Rol FROM Usuarios WHERE Usuario = @usuario", conexion))
+                {
+                    cmd.Parameters.AddWithValue("@usuario", usuario);
+                    conexion.Open();
+
+                    object result = cmd.ExecuteScalar();
+                    if (result != null)
+                    {
+                        rol = result.ToString();
+                    }
+
+                    conexion.Close();
+                }
+
+                return rol;
+
+            }
+        }
+
+
+    }
+}
